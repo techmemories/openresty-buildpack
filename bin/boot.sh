267ee3bbb373ae2b7f6bf2cf54a81d5cd4a6634e
techmemories <bhatnagar.anshul@gmail.com>
2018-01-04 22:24:06 +1100

    Update boot.sh

--- bin/boot.sh
+++ bin/boot.sh
@@ -1,5 +1,5 @@
 export APP_ROOT=$HOME
-export LD_LIBRARY_PATH=$APP_ROOT/openresty/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$APP_ROOT/openresty/lib:$LD_LIBRARY_PATH:$APP_ROOT/openresty/lualib:$APP_ROOT/openresty/luajit
 
 find $APP_ROOT
 
@@ -7,9 +7,9 @@ mkdir -p $APP_ROOT/nginx/logs
 touch $APP_ROOT/nginx/logs/error.log
 touch $APP_ROOT/nginx/logs/access.log
 
-conf_file=$APP_ROOT/openresty/nginx/conf/nginx.conf
+conf_file=$APP_ROOT/openresty/nginx/nginx/conf/nginx.conf
 
-erb $conf_file > $APP_ROOT/openresty/nginx/conf/nginx-final.conf
+erb $conf_file > $APP_ROOT/openresty/nginx/nginx/conf/nginx-final.conf
 
 # ------------------------------------------------------------------------------------------------
 
@@ -19,5 +19,5 @@ mkfifo $APP_ROOT/openresty/nginx/logs/error.log
 cat < $APP_ROOT/openresty/nginx/logs/access.log &
 (>&2 cat) < $APP_ROOT/openresty/nginx/logs/error.log &
 
-exec $APP_ROOT/openresty/nginx/sbin/nginx -p $APP_ROOT/openresty/nginx -c $APP_ROOT/openresty/nginx/conf/nginx-final.conf
+exec $APP_ROOT/openresty/nginx/nginx/sbin/nginx -p $APP_ROOT/openresty/nginx/nginx/ -c $APP_ROOT/openresty/nginx/nginx/conf/nginx-final.conf
 # ------------------------------------------------------------------------------------------------
